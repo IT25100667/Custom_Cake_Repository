64a477f8f554f6e6a4b3d9d3b725c0efb1b94a25
IT25100667 <it25100667@my.sliit.lk>
2026-05-14 12:54:14 +0530

    added service classes, controllers and assigned jsp files

---
+++ OOP_Project-Custom_Cake_Ordering_System-master/src/main/java/com/example/custom_cake_system/controllers/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.example.custom_cake_system.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/authenticate")
+@Controller
+public class AuthenticationController {
+
+}
